84d54498c28dea6b3eab30b0f0769df856ad1834
MaeHoo <maehoo321@gmail.com>
2025-09-04 08:05:43 +0900

    급여

---
+++ Chapter_1/1-1(4).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(void)
+{
+    float num;
+    while (1)
+    {
+        std::cout << "판매 금액을 만원 단위로 입력(-1 to end): ";
+        std::cin >> num;
+        if (num == -1)
+            break;
+        std::cout << "이번 달 급여: " << 50 + (num*0.12) << std::endl;
+    }
+    return 0;
+}
