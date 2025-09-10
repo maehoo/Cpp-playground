e99ba300dee6544aa7205b7299684530febbce44
MaeHoo <maehoo321@gmail.com>
2025-09-10 17:10:47 +0900

    using의 이해

---
+++ Chapter_1/UsingDcl3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int num=20;
+    cout<<"hello world" <<endl;
+    cout<< num << 3.14<<endl;
+    return 0;
+}
+//std에 선언된 모든 것에 대해 이름공간 지정의 생략을 명령할 수 있다.
