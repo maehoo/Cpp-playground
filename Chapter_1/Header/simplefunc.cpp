00b7469491d81c1c80ed7693f8372731aa52ec97
MaeHoo <maehoo321@gmail.com>
2025-09-10 16:21:28 +0900

    정의

---
+++ Chapter_1/Header/simplefunc.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "simplefunc.h"
+
+void bestcomimpl::simplefunc(void)
+{
+    std::cout <<"bestcom이 정의한 함수" << std::endl;
+}
+
+void progcomimpl::simplefunc(void)
+{
+    std::cout<<"progcom이 정의한 함수"<<std::endl;
+}
