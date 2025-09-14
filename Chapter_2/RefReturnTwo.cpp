62de57d72f11aaac48cf231777edc5b6f5aa929d
MaeHoo <maehoo321@gmail.com>
2025-09-14 13:53:07 +0900

    반환형이 참조자인 경우

--- Chapter_2/RefReturnTwo.cpp
+++ Chapter_2/RefReturnTwo.cpp
@@ -18,3 +18,19 @@ int main(void)
     cout <<"num2: "<<num2<<endl;
     return 0;
 }
+/*
+int refretfunctwo(int &ref)
+반환형이 int일 때와 int&일 경우의 차이
+
+
+refretfuncone의 경우 
+int num2= refretfuncone(num1); (O)
+int &num2 = refretfuncone(num1); (O)
+
+
+refretfunctwo의 경우
+int num2 = refretfunctwo(num1); (O)
+int& num2 = refretfunctwo(num1); (X)
+
+의 차이가 있다
+*/
