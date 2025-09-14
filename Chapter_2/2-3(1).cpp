0acf564f5aa68a86af17fb13430bbdb9baaacba0
MaeHoo <maehoo321@gmail.com>
2025-09-14 14:51:51 +0900

    구조체와 new,delete

---
+++ Chapter_2/2-3(1).cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+typedef struct __Point
+{
+    int xpos;
+    int ypos;
+} Point;
+
+Point& pntadder(const Point &p1, const Point &p2)
+{
+    Point * p3 = new Point;
+    (*p3).xpos = p1.xpos + p2.xpos;
+    (*p3).ypos = p1.ypos + p2.ypos;
+    return *p3;
+}
+
+int main(void)
+{
+    Point p1={1,2};
+    Point p2={3,4};
+    Point &p4 = pntadder(p1,p2);
+    cout << p4.xpos << p4.ypos<<endl;
+    delete &p4;
+    return 0;
+}
