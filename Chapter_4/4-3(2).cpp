8f213f021d000649672bee5fd3983e0739d71020
MaeHoo <maehoo321@gmail.com>
2025-09-24 17:25:35 +0900

    생성자,소멸자 활용

---
+++ Chapter_4/4-3(2).cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+namespace comp_pos
+{
+enum
+{
+    clerk,
+    senior,
+    assist,
+    manager
+};
+}
+
+
+class namecard
+{
+    private:
+        char *name;
+        char *company;
+        char *phonenumber;
+        int a;
+    public:
+        namecard(const char *myname, const char *mycompany,const char * myphonenumber,int A):a(A)
+        {
+            int a = strlen(myname)+1;
+            name = new char[a];
+            strcpy(name,myname);
+
+            a = strlen(mycompany) +1;
+            company = new char[a];
+            strcpy(company,mycompany);
+
+            a = strlen(myphonenumber) +1;
+            phonenumber = new char[a];
+            strcpy(phonenumber,myphonenumber);
+
+        }
+        void shownamecardinfo()
+        {
+            cout<<"이름: "<<name<<endl;
+            cout<<"회사: "<<company<<endl;
+            cout<<"전화번호: "<<phonenumber<<endl;
+            switch (a)
+            {
+                case 0:
+                cout<< "직급: 사원"<<endl;
+                break;
+                case 1:
+                cout<< "직급: 주임" <<endl;
+                break;
+                case 2:
+                cout<<"직급: 대리" <<endl;
+                break;
+                case 3:
+                cout<<"직급: 과장"<<endl;
+                break;
+            }
+        }
+        ~namecard()
+        {
+            delete [] name;
+            delete []company;
+            delete []phonenumber;
+        }
+};
+int main(void)
+{
+    namecard manclerk("lee","abceng","010-1111-2222",comp_pos::clerk);
+    namecard mansenior("hong" , "orangeeng" , "010-3333-4444",comp_pos::senior);
+    namecard manassist("kim","sogoodcomp","010-5555-6666",comp_pos::assist);
+    manclerk.shownamecardinfo();
+    mansenior.shownamecardinfo();
+    manassist.shownamecardinfo();
+    return 0;
+}
