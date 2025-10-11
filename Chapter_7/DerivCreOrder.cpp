20e096c4fb2b1f645367d5dca0531b0ab1ec95db
MaeHoo <maehoo321@gmail.com>
2025-10-11 22:44:07 +0900

    상속의 이해

---
+++ Chapter_7/DerivCreOrder.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+class sobase
+{
+    private:
+        int basenum;
+    public:
+        sobase() : basenum(20)
+        {
+            cout<<"sobase()"<<endl;
+        }
+        sobase(int n) :basenum(n)
+        {
+            cout<<"sobase(int n)"<<endl;
+        }
+        void showbasedata()
+        {
+            cout<<basenum<<endl;
+        }
+};
+
+class soderived : public sobase
+{
+    private:
+        int derivnum;
+    public:
+        soderived() : derivnum(30)
+        {
+            cout<<"soderived()"<<endl;
+        }
+        soderived(int n): derivnum(n)
+        {
+            cout<<"soderived(int n)"<<endl;
+        }
+        soderived(int n1, int n2):sobase(n1),derivnum(n2)
+        {
+            cout<<"soderived(int n1, int n2)"<<endl;
+        }
+        void showderivdata()
+        {
+            showbasedata();
+            cout<<derivnum<<endl;
+        }
+};
+
+int main(void)
+{
+    cout<<"case 1...."<<endl;
+    soderived dr1;
+    dr1.showderivdata();
+    cout<<"-----------------------"<<endl;
+    cout<<"case 2...."<<endl;
+    soderived dr2(12);
+    dr2.showderivdata();
+    cout<<"------------------------"<<endl;
+    cout<<"case 3...."<<endl;
+    soderived dr3(23, 24);
+    dr3.showderivdata();
+    return 0;
+}
+/*
+case 1....
+sobase()
+soderived()
+20
+30
+-----------------------
+case 2....
+sobase()
+soderived(int n)
+20
+12
+------------------------
+case 3....
+sobase(int n)
+soderived(int n1, int n2)
+23
+24
+*/
