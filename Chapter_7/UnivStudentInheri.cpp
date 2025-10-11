7b507f81ee25cf640bbcce81a5ac265ee9d11f8e
MaeHoo <maehoo321@gmail.com>
2025-10-11 22:49:10 +0900

    class 상속

--- Chapter_7/UnivStudentInheri.cpp
+++ Chapter_7/UnivStudentInheri.cpp
@@ -48,3 +48,5 @@ int main(void)
     ustd2.whoareyou();
     return 0;
 }
+//상속이 이뤄진 person의 private변수에 직접 접근이 불가능하기 때문에 person 클래스에 정의된 public 함수를
+//통해서 간접적으로 접근을 해야 한다. '정보의 은닉'은 하나의 객체 내에서도 진행이 된다.
