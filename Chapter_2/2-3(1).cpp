b12cf396a824dba1cbcbbfb699d876bbe80f3e19
MaeHoo <maehoo321@gmail.com>
2025-11-01 14:23:24 +0900

    구조체와 new,delete

--- Chapter_2/2-3(1).cpp
+++ Chapter_2/2-3(1).cpp
@@ -17,6 +17,7 @@ Point& pntadder(const Point &p1, const Point &p2)
     (*p3).ypos = p1.ypos + p2.ypos;
     return *p3;
 }
+//p3는 스택에 있는 포인터 변수이고, 힙에 있는 Point 객체의 첫 바이트 주소를 저장한다.
 
 int main(void)
 {
@@ -25,5 +26,6 @@ int main(void)
     Point &p4 = pntadder(p1,p2);
     cout << p4.xpos << p4.ypos<<endl;
     delete &p4;
+    //delete <포인터>; 이기 때문에 'p4'가 아닌 '&p4'가 와야 한다.
     return 0;
 }
