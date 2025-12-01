8f1d2536802634851f7b388939cc76094aeef9b3
MaeHoo <maehoo321@gmail.com>
2025-12-01 11:21:11 +0900

    클래스 템플릿과 static 멤버변수

--- Chapter_14/ClassTemplateStaticMem.cpp
+++ Chapter_14/ClassTemplateStaticMem.cpp
@@ -27,3 +27,13 @@ int main(void)
     obj4.ShowMem();
     return 0;
 }
+/*
+private는 "외부에서 접근(사용)할 수 있냐"를 막는 거지,
+“정의(implementation)를 밖에 쓰면 안 된다”는 뜻이 아니다.
+즉,
+A a;
+
+A::x = 10; ← 이런 “사용”은 private라서 안 됨 (접근 제한)
+
+int A::x = 0; ← 이건 “정의(implementation)”라서 허용됨
+*/
