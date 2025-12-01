bfd329c8d2ffa570642e7d7f3f6d7e40ada1b919
MaeHoo <maehoo321@gmail.com>
2025-12-01 11:20:38 +0900

    템플릿 인자의 이해

--- Chapter_14/NonTypeTemplateParam.cpp
+++ Chapter_14/NonTypeTemplateParam.cpp
@@ -38,5 +38,10 @@ int main(void)
         cout<<i7arr2[i] <<", ";
     cout<<endl;
     return 0;    
-    
 }
+/*
+SimpleArray<int,5>와 SimpleArray<int,7>은 서로 다른 형(type)이다.
+그러므로 i5arr1 = i7arr1은 컴파일 에러가 발생한다.
+생성자를 이용하면 길이가 같은 배열에 대해서만 대입을 허용하기 위해서 추가적인 코드의 삽입이 불가피한데
+템플릿 매개변수를 이용하면 이를 쉽게 해결할 수 있다.
+*/
